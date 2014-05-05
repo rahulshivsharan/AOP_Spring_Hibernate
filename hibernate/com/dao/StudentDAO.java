e39798ed8855df4ec900e5c328f9a0b52ab0bb1f
Rahul Shivsharan <rahulshivsharan@gmail.com>
2014-05-06 00:24:38 +1200

    Create StudentDAO.java

---
+++ hibernate/com/dao/StudentDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hibernate.com.dao.impl;
+
+import java.util.List;
+
+
+public interface StudentDAO<Student> extends BaseDAO<Student> {
+
+    List<Student> addStudent(Student student) throws Exception;
+
+    List<Student> deleteStudent(Integer studentId) throws Exception;
+
+    List<Student> updateStudent(Student student) throws Exception;
+}
