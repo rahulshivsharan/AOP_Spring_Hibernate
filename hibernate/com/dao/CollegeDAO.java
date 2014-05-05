8b42353699faef87ddac954f5c0223ebb34ab23b
Rahul Shivsharan <rahulshivsharan@gmail.com>
2014-05-06 00:25:31 +1200

    Create CollegeDAO.java

---
+++ hibernate/com/dao/CollegeDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hibernate.com.dao.impl;
+
+import java.util.List;
+
+
+public interface CollegeDAO<College> extends BaseDAO<College> {
+
+    List<College> addCollege(College c) throws Exception;
+
+    List<College> deleteCollege(Integer collegeId) throws Exception;
+
+    List<College> updateCollege(College c) throws Exception;
+}
