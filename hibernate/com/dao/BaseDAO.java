2e1e3d9e8ce91bbf4d410fb6620e7e6a129741ca
Rahul Shivsharan <rahulshivsharan@gmail.com>
2014-05-06 00:23:55 +1200

    Create BaseDAO.java

---
+++ hibernate/com/dao/BaseDAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hibernate.com.dao.impl;
+
+import java.util.List;
+
+
+public interface BaseDAO<T> {
+
+    void save(T entity) throws Exception;
+
+    void update(T entity) throws Exception;
+
+    void delete(T entity) throws Exception;
+
+    List<T> list() throws Exception;
+
+    T get(Integer i) throws Exception;
+}
